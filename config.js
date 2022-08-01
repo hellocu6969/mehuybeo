2fe3488d5c111e4bf91051a42f5a1758fe34cd4c
hellocu6969 <dohainam.8g@gmail.com>
2022-08-01 20:17:37 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Ello :33',
+    introDesc: `mmb đúng k hãy trl thật lòng nha :33`,
+    btnIntro: ':O',
+    title: 'Mẹ huy to như con voi mặt như con ếch đúng k :3',
+    desc: 'K tl thì mẹ m béo nha :33 ',
+    btnYes: 'Mẹ Huy năng như con voi mà thằng Huy cũng ngu :33',
+    btnNo: 'Mẹ Huy gầy mà :( ',
+    question:'làm thế nào m biết?',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Ăn như con lợn chả béo',
+    mess: 'T cũng biết mà nhưng mẹ Huy múp với ngon lắm :33',
+    messDesc: 'bảo mẹ e nt cho a nha bé :33',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/hellocu.6969/'
+}
